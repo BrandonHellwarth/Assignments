725e778ade4a32e30a9c1d7073febbf0159cee22
bman9814 <bman9814@gmail.com>
2022-08-04 22:20:45 -0400

    Daikichi Path Variables version 1

---
+++ java/SpringBoot/DaikichiRoutes/src/main/java/com/BrandonHellwarth/DaikichiRoutes/DaikichiRoutesApplication.java
@@ -0,0 +1,12 @@
+package com.BrandonHellwarth.DaikichiRoutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+}
