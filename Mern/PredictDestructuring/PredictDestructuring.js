943a41d0cfb97dd196ee7d10aa383681961ddae2
bman9814 <bman9814@gmail.com>
2022-08-29 17:32:37 -0400

    PredictDestructuring assignment

---
+++ Mern/PredictDestructuring/PredictDestructuring.js
@@ -0,0 +1,21 @@
+//PROBLEM 1
+// Tesla
+// Mercedes
+
+//PROBLEM 2
+//undefined
+//Elon
+
+//PROBLEM 3
+//12345
+//undefined
+
+//PROBLEM 4
+//false
+//true
+
+//PROBLEM 5
+//value
+//[1,5,1,8,3,3]
+//1
+//5
