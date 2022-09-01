fc2b0545be1a8c368838db9c87f9149d0e00c045
bman9814 <bman9814@gmail.com>
2022-08-31 20:49:49 -0400

    First JSX assignment

---
+++ Mern/w1/d3/FirstJSX/first_jsx/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need too do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>finish css on second project</li>
+        <li>host projects with aws</li>
+        <li>update resume</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
