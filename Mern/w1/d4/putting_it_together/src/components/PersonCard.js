b053ed6c6c316931140413c2eb89aaeb9c67799a
bman9814 <bman9814@gmail.com>
2022-09-03 14:14:00 -0400

    refactored component to functional components

--- Mern/w1/d4/putting_it_together/src/components/PersonCard.js
+++ Mern/w1/d4/putting_it_together/src/components/PersonCard.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class PersonCard extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            age : this.props.age
-        }
+const PersonCard = props => {
+    const [state, setState] = useState({
+        age: props.age
+    });
+    const incrementAge = () => { 
+        console.log("working");
+        setState({age : parseInt(state.age) + 1});
     }
-    
-    incrimentAge = () => {
-        this.setState({age : parseInt(this.state.age) + 1});
-    }
-
-    render(){
-        return(
-            <div>
-                <h1>{this.props.firstName}, 
-                {this.props.lastName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={ this.incrimentAge }>Birthday Button for { this.props.firstName } { this.props.lastName }</button>
-            </div>
-        );
-    }
+    return(
+        <div>
+            <h1>{props.firstName}, 
+            {props.lastName}</h1>
+            <p>Age: {state.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+            <button onClick={ incrementAge }>Birthday Button for {props.firstName} {props.lastName}</button>
+        </div>
+    );
 }
 
 export default PersonCard;
